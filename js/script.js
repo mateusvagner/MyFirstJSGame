ae6c7ad67cd4a29c02c939c272b206c126f6502c
Mateus <you@example.com>
2020-04-18 21:33:11 -0300

    hello world working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-var config = {
+(function () {
+    var config = {
         type: Phaser.AUTO,
         width: 800,
         height: 600,
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: { y: 200 }
+                gravity: {
+                    y: 200
+                }
             }
         },
         scene: {
@@ -16,24 +19,25 @@ var config = {
 
     var game = new Phaser.Game(config);
 
-    function preload ()
-    {
-        this.load.setBaseURL('http://labs.phaser.io');
+    function preload() {
+        this.load.setBaseURL('http://labs.phaser.io/');
 
         this.load.image('sky', 'assets/skies/space3.png');
         this.load.image('logo', 'assets/sprites/phaser3-logo.png');
         this.load.image('red', 'assets/particles/red.png');
     }
 
-    function create ()
-    {
+    function create() {
         this.add.image(400, 300, 'sky');
 
         var particles = this.add.particles('red');
 
         var emitter = particles.createEmitter({
             speed: 100,
-            scale: { start: 1, end: 0 },
+            scale: {
+                start: 1,
+                end: 0
+            },
             blendMode: 'ADD'
         });
 
@@ -45,3 +49,4 @@ var config = {
 
         emitter.startFollow(logo);
     }
+}());
